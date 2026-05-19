f94c783980cc26dadc3d7bfcf697f744d696f6fe
linxutao <linxutao_1@qq.com>
2026-05-19 17:00:19 +0800

    测试文件

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        int a = 39;
+        int b = 20;
+        System.out.println(a+b);
         System.out.println("Hello world!");
     }
 }
